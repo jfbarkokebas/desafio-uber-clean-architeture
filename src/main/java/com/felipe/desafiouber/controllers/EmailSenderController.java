0752606abd0b0fc3ba9e68e3f69b49cfc9110922
felipe <jfbarkokebas@gmail.com>
2024-02-18 13:04:48 -0300

    create function sendEmail in EmailSenderController

--- src/main/java/com/felipe/desafiouber/controllers/EmailSenderController.java
+++ src/main/java/com/felipe/desafiouber/controllers/EmailSenderController.java
@@ -1,9 +1,14 @@
 package com.felipe.desafiouber.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipe.desafiouber.application.EmailSenderService;
+import com.felipe.desafiouber.core.exceptions.EmailServiceException;
 
 @RestController
 @RequestMapping("/api/email")
@@ -13,6 +18,18 @@ public class EmailSenderController {
 
     public EmailSenderController(EmailSenderService emailService) {
         this.emailSenderService = emailService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request){
+        try {
+            
+            this.emailSenderService.sendEmail(request.to(), request.subject(), request.body());
+            return ResponseEntity.ok("email send successfully");
+
+        } catch (EmailServiceException exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while send email");
+        }
     }
 
     
